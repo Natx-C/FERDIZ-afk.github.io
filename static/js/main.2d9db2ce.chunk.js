d240d04e551cf9ff7159e0a25ddd8524cb52249c
FERDIZ-afk <ferdizakyla@gmail.com>
2022-02-14 19:08:01 +0700

    千夜未来

--- static/js/main.2d9db2ce.chunk.js
+++ static/js/main.2d9db2ce.chunk.js
@@ -54,7 +54,7 @@
                     default: "light",
                     disableSwitch: false,
                     respectPrefersColorScheme: false,
-                   themes: ["light", "dark"]
+                   themes: ["light"]
                   // themes: ["light", "dark", "cupcake", "bumblebee", "emerald", "corporate", "synthwave", "retro", "cyberpunk", "valentine", "halloween", "garden", "forest", "aqua", "lofi", "pastel", "fantasy", "wireframe", "black", "luxury", "dracula"]
                 }
             }
